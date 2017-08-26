1cfc6b61e5cc504cdc47ecadddb1bb5df8addd0c
Jesse Hughes <revelstokesnowboard@gmail.com>
2017-08-25 22:37:32 -0700

    Cleanup enums

--- bugstats.hs
+++ bugstats.hs
@@ -2,57 +2,55 @@ import Data.Char as Char
 
 main = do
   fileContents <- readFile "bugs.md"
-  let tokens = tokenizeStream fileContents
-  let bugs = lexTokens tokens []
+  let tokens = tokStream fileContents
+  let bugs = mkBugsFromTokens tokens []
   print bugs
 
 
-
-tokenizeStream :: [Char] -> [Token]
-tokenizeStream [] = []
-tokenizeStream (char : charStream) = tokenizeChar char : tokenizeStream charStream
-
-
-tokenizeChar :: Char -> Token
-tokenizeChar c
-  | c == '#' = Hash
-  | c == '@' = At
-  | c == ':' = Colon
-  | c == '-' = Dash
-  | c == ' ' = Whitespace
-  | c == '\t' = Whitespace
-  | c == '\n' = Newline
-  | isDigit c = Digit c
-  | isAlpha c = Alpha c
-  | otherwise = Undefined c
+tokStream :: [Char] -> [Token]
+tokStream [] = []
+tokStream (char : charStream) = tokChar char : tokStream charStream
 
 
-lexTokens :: [Token] -> [Bug] -> [Bug]
-lexTokens [] _ = []
-lexTokens (token : tokenList) bugs
-  | token == Hash = mkBug tokenList : bugs ++ lexTokens tokenList bugs
-  | otherwise = lexTokens tokenList bugs
+tokChar :: Char -> Token
+tokChar c
+  | c == '#' = Token_StartBugHeader
+  | c == '*' = Token_StartBugTag
+  | c == '-' = Token_Dash
+  | c == ' ' = Token_Whitespace
+  | c == '\t' = Token_Whitespace
+  | c == '\n' = Token_Newline
+  | otherwise = Token_Undefined
 
+
+mkBugsFromTokens :: [Token] -> [Bug] -> [Bug]
+mkBugsFromTokens [] _ = []
+mkBugsFromTokens all@(token : tokenList) bugList
+  | token == Token_StartBugHeader = mkBug all : bugList
+  -- | token == Token_StartBugTag    = mkBug tokenList : bugs ++ mkBugsFromTokens(tokenList bugs)
+  -- | otherwise = mkBugsFromTokens(tokenList bugs)
 
 mkBug :: [Token] -> Bug
-mkBug _ = Bug "" ""
+mkBug (token : list)
+  | token == Token_StartBugHeader = mkBugHeader (list, Bug "Start")
+  | token == Token_Undefined = Bug "Undefined"
+  | token == Token_Whitespace = Bug "Whitespace"
+  | otherwise = Bug "Otherwise"
+
+mkBugHeader :: ([Token], Bug) -> Bug
+mkBugHeader ((token : list), bug) = bug
 
 
-data Token = Hash
-           | At
-           | Colon
-           | Dash
-           | Newline
-           | Whitespace
-           | Digit Char
-           | Alpha Char
-           | Undefined Char
+
+data Token = Token_StartBugHeader
+           | Token_StartBugTag
+           | Token_Dash
+           | Token_Newline
+           | Token_Whitespace
+           | Token_Undefined
   deriving (Show, Eq)
 
 
-data Bug =
-  Bug { header :: String,
-        description :: String }
-  deriving (Show)
+data Bug = Bug String deriving (Show)
 
 
